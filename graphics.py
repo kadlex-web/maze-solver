f5e86cc403ef7a4a765eb2d8025aa335cef24191
kadlex <akulp18@gmail.com>
2025-03-21 15:31:36 -0400

    added Cell Class to draw cells based on coordinates and if certain walls exist

--- graphics.py
+++ graphics.py
@@ -24,7 +24,7 @@ class Window:
     def close(self):
         self.is_running = False
 
-    def draw(self, line, fill_color):
+    def draw(self, line, fill_color="black"):
         line.draw(self.canvas, fill_color)
 
 class Point:
@@ -38,7 +38,47 @@ class Line:
         self.point1 = point1 # first point object
         self.point2 = point2 # second point object
 
-    # takes the values of the points and then draws a line between the two of them...I think?
-    def draw(self,canvas, fill_color):
-        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2
-)
+    # takes the values of the points and then draws a line between the two of them
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
+
+class Cell:
+    # Class which defines a cell. 
+    # It should know which walls it has, where it exists on the canvas in x/y coordinates, and access to the window so that it can draw itself
+    def __init__(self, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = window
+
+    def draw(self, x1, x2, y1, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            left_wall = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw(left_wall)
+
+        if self.has_right_wall:
+            right_wall = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw(right_wall)
+
+        if self.has_top_wall:
+            top_wall = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw(top_wall)
+
+        if self.has_bottom_wall:
+            bottom_wall = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw(bottom_wall)
+
+'''
+(1,1)      (2,1)
+
+(1,2)      (2,2)
+'''
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from graphics import Window, Line, Point
+from graphics import Window, Line, Point, Cell
 
 def main():
     window = Window(800, 600)
-    line1 = Line(Point(100,200), Point(200,300))
-    line2 = Line(Point(400,400), Point(100,100))
-    window.draw(line1, "black")
-    window.draw(line2, "blue")
+
+    cell1 = Cell(window)
+    cell1.has_bottom_wall = False
+    cell1.has_top_wall = False
+    cell1.draw(100, 200, 200, 300)
+
+    cell2 = Cell(window)
+    cell2.has_right_wall = False
+    cell2.has_left_wall = False
+    cell2.draw(200, 300, 300, 400)
+
     window.wait_for_close()
 
 main()
